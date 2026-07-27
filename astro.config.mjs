0a41a5426c42b86c3d52b2b9e94782341c9d983d
jnsahani2017-creator <jnsahani2017@gmail.com>
2026-07-27 14:49:38 +0530

    Update astro.config.mjs
    import { defineConfig } from 'astro/config';
    import tailwind from '@astrojs/tailwind';
    import sitemap from '@astrojs/sitemap';
    
    export default defineConfig({
      site: 'https://jnsahani2017-creator.github.io/aimezik-website/',
      output: 'static',
      outDir: './docs',
      integrations: [tailwind(), sitemap()]
    });

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,1 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
+export default defineConfig({
+  site: 'https://jnsahani2017-creator.github.io/aimezik-website/',
+  output: 'static',
+  outDir: './docs',
+  integrations: [tailwind(), sitemap()]
+});
